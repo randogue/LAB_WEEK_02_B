14ad7910347d23ed1b5433ac349553d3eaf6c92c
randogue <121857862+randogue@users.noreply.github.com>
2025-09-15 22:42:40 +0700

    Commit No. 3 - Fix application crash when user input wrong color code [Intent]

--- app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val COLOR_KEY = "COLOR_KEY"
     }
+
+    //added for ActivityResultLauncher
+    private lateinit var launcher: ActivityResultLauncher<Intent> //in kotlin, you specify data type from colon ':'
 
     private val submitButton: Button
         get() = findViewById<Button>(R.id.submit_button) //findViewById(R.id.submit_button) will work just fine since the compiler can easily tell if it's a button type
@@ -30,20 +36,54 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+
+        //added for ActivityResultLauncher, make a class to handle the result passed from second Act
+        val resultCallback = object: ActivityResultCallback<ActivityResult> { //ActivityResultCallback<ActivityResult> is an interface
+            override fun onActivityResult(result: ActivityResult) { //method override
+                val data: Intent? = result.data //data is nullable, pull ActivityResult.data
+                if (data != null) { //to ensure that data is not null while executing
+                    val error = data?.getBooleanExtra("ERROR_KEY", false) //get data with name:"ERROR_KEY", in case data is not found, use default value (from the second param)
+
+                    if (error == true) { //if boolean is true
+                        //show pop up window warning
+                        Toast.makeText(this@MainActivity,
+                            getString(R.string.color_code_input_invalid),
+                            Toast.LENGTH_LONG
+                            ).show()
+                    }
+                }
+            }
+        }
+
+
+        //Main content of activity
         submitButton.setOnClickListener {
             val colorCode =
                 findViewById<TextInputEditText>(R.id.color_code_input_field).text.toString()
             if (colorCode.isNotEmpty()) { //when field is not empty
-                if (colorCode.length > 6) { //when length is too long
-                    Toast.makeText(
-                        this,
-                        getString(R.string.color_code_input_wrong_length),
-                        Toast.LENGTH_LONG
-                    ).show()
+                if (colorCode.length != 6) { //when length is too long
+                    if (colorCode.length > 6) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            getString(R.string.color_code_input_wrong_length),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            getString(R.string.color_code_input_wrong_length),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
-                else { //when length is not too long
-                    val ResultIntent = Intent(this, ResultActivity::class.java) //create new intent(context:this activity, class_of_activity)
-                    ResultIntent.putExtra("COLOR_KEY",colorCode) //pass a value with certain name
+                else if (colorCode.length == 6) { //when length is just right
+                    val ResultIntent = Intent(
+                        this@MainActivity,
+                        ResultActivity::class.java) //create new intent(context:this activity, class_of_activity)
+                    ResultIntent.putExtra(
+                        "COLOR_KEY",
+                        colorCode) //pass a value with certain name
                     startActivity(ResultIntent)
                 }
             }
--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -1,5 +1,7 @@
 package com.example.lab_week_02_b
 
+import android.app.Activity
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.TextView
@@ -10,9 +12,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ResultActivity : AppCompatActivity() {
-    companion object {
+    companion object { //was not used in order to make future me undestand how some function works
         private const val COLOR_KEY = "COLOR_KEY"
-    }
+        private const val ERROR_KEY = "ERROR_KEY"
+    } //basically these are like aliases (in this case as a variable that contains string as key) so that any changes will reflect to all keys.
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,13 +28,36 @@ class ResultActivity : AppCompatActivity() {
             insets
         }
 
-        if (intent != null) {
+
+        if (intent != null) { //when accessed via intent
             val colorCode = intent.getStringExtra("COLOR_KEY") //get the extra passed through intent
 
             val bgScreenDW = findViewById<ConstraintLayout>(R.id.background_screen) //xml layouts are ConstraintLayout
-            bgScreenDW.setBackgroundColor(Color.parseColor("#$colorCode")) //$ is used to insert str variable into "quotation/string"
-            val resultMSG = findViewById<TextView>(R.id.color_code_result_message)
-            resultMSG.text = getString(R.string.color_code_result_message,colorCode?.uppercase()) //%s in xml is for formatting
+            try {
+                bgScreenDW.setBackgroundColor(
+                    Color.parseColor("#$colorCode")
+                ) //$ is used to insert str variable into "quotation/string"
+            }
+            catch(exception : IllegalArgumentException) {
+                val resultIntent = Intent() //create an Intent Object
+                resultIntent.putExtra(
+                    "ERROR_KEY",
+                    true
+                ) //put value to "ERROR_KEY"
+                setResult(
+                    Activity.RESULT_OK,
+                    resultIntent
+                ) //send intent object with the status (-1 means success or RESULT_OK)
+                finish() //closes current activity, then sends it to the back stack or destroy it.
+                //basically finish() close this activity and go back to the previous existing Activity (in this case its Main Activity).
+            }
+            val resultMSG = findViewById<TextView>(
+                R.id.color_code_result_message
+            )
+            resultMSG.text = getString(
+                R.string.color_code_result_message,
+                colorCode?.uppercase()
+            ) //%s in xml is for formatting
         }
     }
 
